903202da21027f297915e73b9b845167f5f82d23
Vitaliy Smetana <vitaliys.smetana@gmail.com>
2019-06-27 18:06:43 +0300

    Add base services

---
+++ Helsi.DomainLogic/Services/Abstraction/BaseService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Helsi.DomainLogic.Services
+{
+    public class BaseService<T> : IBaseService<T> where T : class
+    {
+        protected readonly DbContext _context;
+
+        public void Commit(bool saveChanges = true)
+        {
+            if (saveChanges)
+                _context.SaveChanges();
+        }
+
+        public T Create(T newItem, bool shouldBeCommited = true)
+        {
+            _context.Set<T>().Add(newItem);
+            Commit(shouldBeCommited);
+
+            return newItem;
+        }
+
+        public T Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _context.Set<T>();
+        }
+
+        public void Remove(int id, bool shouldBeCommited = true)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Helsi.DomainLogic/Services/Abstraction/IBaseService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Helsi.DomainLogic.Services
+{
+    public interface IBaseService<T> where T : class
+    {
+        T Get(int id);
+        T Create(T newItem, bool shouldBeCommited = true);
+        IQueryable<T> GetAll();
+        void Remove(int id, bool shouldBeCommited = true);
+        void Commit(bool saveChanges = true);
+    }
+}
