1648ca24fcc78327f986e42feadb2eeab2436178
Vitaliy Smetana <vitaliys.smetana@gmail.com>
2019-06-27 17:36:19 +0300

    Add dto

---
+++ Helsi.Dto/AdditionalContactDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helsi.Dto
+{
+    public class AdditionalContactDto
+    {
+        public int Id { get; set; }
+        public int ContactTypeId { get; set; }
+        public int PatientId { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public ContactTypeDto ContactType { get; set; }
+        public PatientDto Patient { get; set; }
+    }
+}
---
+++ Helsi.Dto/PatientDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Helsi.Dto
+{
+    public class PatientDto
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int GenderId { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public GenderDto Gender { get; set; }
+        public ICollection<AdditionalContactDto> AdditionalContactDtos { get; set; }
+    }
+}
