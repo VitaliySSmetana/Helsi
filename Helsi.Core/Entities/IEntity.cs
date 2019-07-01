5064c1c1d1b70cdc5ad07778e0335268d9a64095
Vitaliy Smetana <vitaliys.smetana@gmail.com>
2019-07-01 12:47:18 +0300

    Add core project

---
+++ Helsi.Core/Entities/IEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helsi.Core.Entities
+{
+    public interface IEntity : IEntity<int>
+    {
+        new int Id { get; set; }
+    }
+
+    public interface IEntity<T>
+    {
+        T Id { get; set; }
+    }
+}
