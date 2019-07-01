41ef5f69153e81a8651a0d73be9c3977746a8236
Vitaliy Smetana <vitaliys.smetana@gmail.com>
2019-07-01 17:47:19 +0300

    Add active field for patient

--- Helsi.DataAccess/Models/Patient.cs
+++ Helsi.DataAccess/Models/Patient.cs
@@ -17,6 +17,7 @@ namespace Helsi.DataAccess.Models
         public DateTime BirthDate { get; set; }
         public int GenderId { get; set; }
         public string PhoneNumber { get; set; }
+        public bool IsActive { get; set; }
 
         public virtual Gender Gender { get; set; }
         public virtual ICollection<AdditionalContact> AdditionalContacts { get; set; }
