f0133bb9ee68ed022cc792fa27f527ca73c3fa49
Vitaliy Smetana <vitaliys.smetana@gmail.com>
2019-07-01 18:09:02 +0300

    Update patient dto

--- Helsi.Dto/PatientDto.cs
+++ Helsi.Dto/PatientDto.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Helsi.Dto
 {
     public class PatientDto
     {
         public int Id { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string Patronymic { get; set; }
+        [Required]
         public DateTime BirthDate { get; set; }
+        [Required]
         public int GenderId { get; set; }
+        [Required]
         public string PhoneNumber { get; set; }
+        [Required]
         public bool IsActive { get; set; }
         public GenderDto Gender { get; set; }
         public ICollection<AdditionalContactDto> AdditionalContacts { get; set; }
