4236a5273c5ac91e591389e9410ed5296701fd4d
Vitaliy Smetana <vitaliys.smetana@gmail.com>
2019-07-03 14:43:32 +0300

    Add validation for patient

---
+++ Helsi.Dto/Attributes/CustomDateRangeAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Helsi.Dto.Attributes
+{
+    public class CustomDateRangeAttribute : ValidationAttribute
+    {
+        private DateTime MinDate { get; set; }
+        private DateTime MaxDate { get; set; }
+
+        public CustomDateRangeAttribute(string minDate)
+        {
+            MinDate = Convert.ToDateTime(minDate);
+            MaxDate = DateTime.Today;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var date = (DateTime)value;
+
+            if (date >= MinDate && date <= MaxDate)
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult($"Date must be since {MinDate.ToString()} till {MaxDate.ToString()}");
+            }
+        }
+    }
+}
--- Helsi.Dto/PatientDto.cs
+++ Helsi.Dto/PatientDto.cs
@@ -1,27 +1,45 @@
-﻿using System;
+﻿using Helsi.Dto.Attributes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+
 
 namespace Helsi.Dto
 {
     public class PatientDto
     {
         public int Id { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Last name is required")]
+        [StringLength(128, ErrorMessage = "Max length for {0} is 128 characters")]
+        [RegularExpression("@^([А-ЯЁа-яё -])$")]
         public string LastName { get; set; }
+
         [Required]
+        [StringLength(128, ErrorMessage = "Max length for {0} is 128 characters")]
+        [RegularExpression("@^([А-ЯЁа-яё -])$")]
         public string FirstName { get; set; }
-        [Required]
+
+        [StringLength(128, ErrorMessage = "Max length for {0} is 128 characters")]
+        [RegularExpression("@^([А-ЯЁа-яё -])$")]
         public string Patronymic { get; set; }
+
         [Required]
+        [DataType(DataType.Date)]
+        [CustomDateRange("1/1/1900")]
         public DateTime BirthDate { get; set; }
+
         [Required]
         public int GenderId { get; set; }
+
         [Required]
+        [RegularExpression(@"^(\+)(\d{12})$")]
         public string PhoneNumber { get; set; }
-        [Required]
+
         public bool IsActive { get; set; }
+
         public GenderDto Gender { get; set; }
+
         public ICollection<AdditionalContactDto> AdditionalContacts { get; set; }
     }
 }
