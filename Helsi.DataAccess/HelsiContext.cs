6595562f77df5f158855e3d9292dec4dcd1adaf2
Vitaliy Smetana <vitaliys.smetana@gmail.com>
2019-07-04 12:56:14 +0300

    Update dbSet declaration

--- Helsi.DataAccess/HelsiContext.cs
+++ Helsi.DataAccess/HelsiContext.cs
@@ -13,10 +13,10 @@ namespace Helsi.DataAccess
 
         }
 
-        public DbSet<Patient> Patients { get; set; }
-        public DbSet<Gender> Genders { get; set; }
-        public DbSet<AdditionalContact> AdditionalContacts { get; set; }
-        public DbSet<ContactType> ContactTypes { get; set; }
+        public virtual DbSet<Patient> Patients { get; set; }
+        public virtual DbSet<Gender> Genders { get; set; }
+        public virtual DbSet<AdditionalContact> AdditionalContacts { get; set; }
+        public virtual DbSet<ContactType> ContactTypes { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
